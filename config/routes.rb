ee7f00a2ed353413a4e86022e1f0fdd6c84b1bb4
Yuki Akino <yuki7736@gmail.com>
2025-04-08 11:48:10 +0900

    Remove OmniAuth integration (not used for now)

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,5 @@
 Rails.application.routes.draw do
   devise_for :users, controllers: {
-    omniauth_callbacks: "users/omniauth_callbacks",
     registrations: "users/registrations"
   }
   get "home/top"
