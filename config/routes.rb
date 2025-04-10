ba46bb17f8450d16dfc32068138731d1880c13ac
Yuki Akino <yuki7736@gmail.com>
2025-04-10 23:50:09 +0900

    アプリ説明ページ実装

--- config/routes.rb
+++ config/routes.rb
@@ -2,17 +2,6 @@ Rails.application.routes.draw do
   devise_for :users, controllers: {
     registrations: "users/registrations"
   }
-  get "home/top"
-  # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
-  # Reveal health status on /up that returns 200 if the app boots with no exceptions, otherwise 500.
-  # Can be used by load balancers and uptime monitors to verify that the app is live.
-  get "up" => "rails/health#show", as: :rails_health_check
-
-  # Render dynamic PWA files from app/views/pwa/* (remember to link manifest in application.html.erb)
-  # get "manifest" => "rails/pwa#manifest", as: :pwa_manifest
-  # get "service-worker" => "rails/pwa#service_worker", as: :pwa_service_worker
-
-  # Defines the root path route ("/")
-  # root "posts#index"
-  root "home#top"
+  root 'home#top'
+  get 'about', to: 'home#about', as: :about
 end
